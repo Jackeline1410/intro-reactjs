266f5bdc098fcda1c50cd8ccc03c469688d9249e
Jackeline1410 <ing.jacke@gmail.com>
2021-09-13 12:15:23 -0500

    Botones del formulario de añadir un nuevo TODO

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -4,6 +4,7 @@ import { TodoCounter } from '../TodoCounter';
 import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
 import { TodoItem } from '../TodoItem';
+import { TodoForm } from '../TodoForm';
 import { CreateTodoButton } from '../CreateTodoButton';
 import { Modal } from '../Modal';
 
@@ -16,7 +17,7 @@ function AppUI(){
     deleteTodo,
     openModal,
     setOpenModal,
-  }= React.useContext(TodoContext);
+  } = React.useContext(TodoContext);
 
   return (
   <React.Fragment>
@@ -41,7 +42,7 @@ function AppUI(){
 
           {!!openModal && (
             <Modal>
-            <p>{searchedTodos[0]?.text}</p>
+            <TodoForm />
           </Modal>
           )}
 
