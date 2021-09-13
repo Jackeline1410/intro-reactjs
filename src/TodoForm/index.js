ce59b38dce17fbd92a42ebe0e39d6ccddedb387d
Jackeline1410 <ing.jacke@gmail.com>
2021-09-13 16:13:17 -0500

    Añadiendo estilos al componente

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TodoContext } from '../TodoContext';
+import './TodoForm.css';
 
 function TodoForm() {
     const [ newTodoValue, setNewTodoValue ] = React.useState('');
